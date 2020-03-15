1426f130211b05ddf95fa41ff2a739f9c0368cf4
David Stout <davidgstout63@gmail.com>
2020-03-15 16:54:53 -0400

    Initial commit. Basic changes to template_directory. Nothing changed in header/footer/etc.

---
+++ index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+    	<meta charset="utf-8">
+    	<meta name="description" content="Lorem Dental Lab">
+    	<meta name="keywords" content="Dentures">
+		<title><?php bloginfo( 'name'); ?> | <?php bloginfo( 'description'); ?></title>
+		<link rel="stylesheet" href="<?php bloginfo(template_directory); ?>/css/normalize.css">
+		<link rel="stylesheet" href="<?php bloginfo(template_directory); ?>/css/style.css">
+	</head>
+	<body>
+
+
+	
+		<div id="header">
+			<h1><a href="#">Lorem Dental</a></h1>
+		</div><!-- #header --> 
+
+		<div class="navcontainer">
+			<div class="inner-navcontainer">
+				<nav class="clearfix">
+						<ul>
+							<li><a href="#">Home</a></li>
+							<li><a href="#">PPE and Safety</a></li>
+							<li><a href="#">COVID-19 and Dentistry</a></li>
+							<li><a href="#">ADA Recommendations</a></li>
+							<li><a href="#">About Us</a></li>
+						</ul>
+				</nav>
+			</div>
+		</div>
+
+
+
+
+		<div id="container">
+			
+			<div id="main">
+	
+				<div class="post">
+					<h2><a href="#">Mask Shortages: Statement from CEO</a></h2>
+					<img src="<?php bloginfo(template_directory); ?>images/masks.png" alt="Dentist in mask">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a consectetur purus. Pellentesque at leo erat, id pellentesque sapien. Cras id purus sem. Nulla lacinia consequat nulla, vitae porttitor urna venenatis id. Nam iaculis risus ac mauris vestibulum malesuada. Nulla dolor mi, cursus sed ornare vel, feugiat sed nibh. Suspendisse vitae pulvinar ligula. In eu dui erat. Etiam sit amet malesuada turpis. Duis dapibus feugiat euismod. Suspendisse vitae consequat nunc. Vivamus aliquet vulputate varius.</p>
+					
+					<span class="post-meta">Posted by <a href="#">David Stout</a> on March 11</span>
+
+				</div><!-- .post --> 
+
+				<div class="post">
+					<h2><a href="#">Chair Sanitizing: What You Need to Know</a></h2>
+					<img src="<?php bloginfo(template_directory); ?>images/chairs.png" alt="Dental patient in chair">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a consectetur purus. Pellentesque at leo erat, id pellentesque sapien. Cras id purus sem. Nulla lacinia consequat nulla, vitae porttitor urna venenatis id. Nam iaculis risus ac mauris vestibulum malesuada. Nulla dolor mi, cursus sed ornare vel, feugiat sed nibh. Suspendisse vitae pulvinar ligula. In eu dui erat. Etiam sit amet malesuada turpis. Duis dapibus feugiat euismod. Suspendisse vitae consequat nunc. Vivamus aliquet vulputate varius. Boomer lives!</p>	
+
+					<span class="post-meta">Posted by <a href="#">David Stout</a> on March 10</span>
+				</div><!-- .post --> 
+
+			</div><!-- #main --> 
+
+
+
+
+
+			<div id="sidebar" class="clearfix">
+
+				<div class="widget">
+					<h3>Archive</h3>
+					<ul>
+						<li><a href="#">January</a></li>
+						<li><a href="#">February</a></li>
+						<li><a href="#">March</a></li>
+						<li><a href="#">April</a></li>
+						<li><a href="#">May</a></li>
+						<li><a href="#">June</a></li>
+						<li><a href="#">July</a></li>
+					</ul>
+				</div><!-- .widget --> 
+
+				<div class="widget">
+					<h3>PPE Suppliers</h3>
+					<ul>
+						<li><a href="#">Filler Text</a></li>
+						<li><a href="#">Filler Text</a></li>
+						<li><a href="#">Filler Text</a></li>
+						<li><a href="#">Filler Text</a></li>
+						<li><a href="#">Filler Text</a></li>
+					</ul>
+				</div><!-- .widget --> 
+
+				<div class="widget">
+					<a href="#">
+						<img src="images/ad-submit-your-own.png" alt="Filler Text>
+					</a>
+				</div><!-- .widget --> 
+
+				<div class="widget">
+					<a href="#">
+						<img src="images/ad-tshirts.png" alt="Filler Text">
+					</a>
+				</div><!-- .widget --> 
+
+
+			</div><!-- #sidebar -->
+
+			
+			<div class="clear"></div>
+			<div id="footer">
+
+				<a href="#">
+					<img src="images/social-icon-twitter.png" alt="Twitter">
+				</a>
+
+				<a href="#">
+					<img src="images/social-icon-facebook.png" alt="Facebook">
+				</a>
+				
+			</div><!-- #footer -->
+
+		</div><!-- #container --> 
+	</body>
+</html>
