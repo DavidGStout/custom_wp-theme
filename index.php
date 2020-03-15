9f804f51f79bf9dbe8ad8ce2782dcdd894d7b1b5
David Stout <davidgstout63@gmail.com>
2020-03-15 18:54:31 -0400

    Fixed broken image path "temporarily" with URL. Added favicon.

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 		<link rel="stylesheet" href="<?php bloginfo(template_directory); ?>/css/style.css">
 	</head>
 	<body>
+		<link rel="icon" href="favicon.png" type="image/gif" sizes="16x16">
 
 
 	
@@ -38,9 +39,10 @@
 			<div id="main">
 	
 				<div class="post">
+					
 					<h2><a href="#">Mask Shortages: Statement from CEO</a></h2>
-					<img src="<?php bloginfo(template_directory); ?>images/masks.png" alt="Dentist in mask">
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a consectetur purus. Pellentesque at leo erat, id pellentesque sapien. Cras id purus sem. Nulla lacinia consequat nulla, vitae porttitor urna venenatis id. Nam iaculis risus ac mauris vestibulum malesuada. Nulla dolor mi, cursus sed ornare vel, feugiat sed nibh. Suspendisse vitae pulvinar ligula. In eu dui erat. Etiam sit amet malesuada turpis. Duis dapibus feugiat euismod. Suspendisse vitae consequat nunc. Vivamus aliquet vulputate varius.</p>
+					<img src="https://cdn.pixabay.com/photo/2014/12/10/21/01/doctor-563429__340.jpg">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a consectetur purus. Pellentesque at leo erat, id pellentesque sapien. Cras id purus sem. Nullu lacinia consequat nulla, vitae porttitor urna venenatis id. Nam iaculis risus ac mauris vestibulum malesuada. Nulla dolor mi, cursus sed ornare vel, feugiat sed nibh. Suspendisse vitae pulvinar ligula. In eu dui erat. Etiam sit amet malesuada turpis. Duis dapibus feugiat euismod. Suspendisse vitae consequat nunc. Vivamus aliquet vulputate varius.</p>
 					
 					<span class="post-meta">Posted by <a href="#">David Stout</a> on March 11</span>
 
@@ -48,7 +50,7 @@
 
 				<div class="post">
 					<h2><a href="#">Chair Sanitizing: What You Need to Know</a></h2>
-					<img src="<?php bloginfo(template_directory); ?>images/chairs.png" alt="Dental patient in chair">
+					<img src="https://cdn.pixabay.com/photo/2017/08/06/07/12/chair-2589771__340.jpg" alt="Dental patient in chair">
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a consectetur purus. Pellentesque at leo erat, id pellentesque sapien. Cras id purus sem. Nulla lacinia consequat nulla, vitae porttitor urna venenatis id. Nam iaculis risus ac mauris vestibulum malesuada. Nulla dolor mi, cursus sed ornare vel, feugiat sed nibh. Suspendisse vitae pulvinar ligula. In eu dui erat. Etiam sit amet malesuada turpis. Duis dapibus feugiat euismod. Suspendisse vitae consequat nunc. Vivamus aliquet vulputate varius. Boomer lives!</p>	
 
 					<span class="post-meta">Posted by <a href="#">David Stout</a> on March 10</span>
